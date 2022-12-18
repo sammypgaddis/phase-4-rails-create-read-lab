87f5a248e65433b896d2de6029faa36cb9b1d592
Samuel Gaddis <siddag14@live.com>
2022-12-18 16:32:23 -0500

    commit tree 50c111def68cdac2f5c2ccc13408a5ba7a41179f

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,15 @@
+class PlantsController < ApplicationController
+
+  def index
+    plants = Plant.all 
+    render json: plants
+  end
+
+  def show
+    plant = Plant.find_by(id: params[:id])
+    render json: plant
+  end
+
+  def create
+  end
+end
